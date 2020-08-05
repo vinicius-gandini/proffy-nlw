045e39ed68774a31a698b6b639f506ad183d5936
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-08-05 11:48:16 -0300

    Metade da aula finalizada. Criado componente Select :ok_hand:

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+import Select from '../../components/Select';
+
 function TeacherForm() {
    return (
-      <div id="page-teacher-list" className="container">
-         <PageHeader title="Que incrível que você quer dar aulas" />
+      <div id="page-teacher-form" className="container">
+         <PageHeader 
+            title="Que incrível que você quer dar aulas" 
+            description="O primeiro passo é preencher esse formulário de inscrição"
+         />
+
+         <main>
+            <fieldset>
+               <legend>Seus Dados</legend>
+
+               <Input name="name" label="Nome completo" />
+               <Input name="avatar" label="Avatar" />
+               <Input name="whatsapp" label="Whatsapp" />
+               <Textarea name="bio" label="Biografia"/>   
+            </fieldset>
+
+            <fieldset>
+               <legend>Sobre a Aula</legend>
+
+               <Select 
+               name="subject" 
+               label="Matéria" 
+               options={[
+                  { value: 'Artes', label: 'Artes' },
+                  { value: 'Biologia', label: 'Biologia' },
+                  { value: 'Ciências', label: 'Ciências' },
+                  { value: 'Educação Fìsica', label: 'Educação Fìsica' },
+                  { value: 'Física', label: 'Física' },
+                  { value: 'Geografia', label: 'Geografia' },
+                  { value: 'História', label: 'História' },
+                  { value: 'Matemática', label: 'Matemática' },
+                  { value: 'Português', label: 'Português' },
+                  { value: 'Química', label: 'Química' },
+               ]}
+               />
+               <Input name="cost" label="Csuto da sua hora por aula" />
+            </fieldset>
+
+            <footer>
+               <p>
+                  <img src={warningIcon} alt="Aviso importante"/>
+                  Importante! <br />
+                  Preencha todos os dados
+               </p>
+               <button type="button" >Salvar</button>
+            </footer>
+         </main>
       </div>
    );
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,26 +3,16 @@ import './styles.css';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 function TeacherList() {
    return (
       <div id="page-teacher-list" className="container">
          <PageHeader title="Estes são os proffys disponíveis">
             <form id="search-teachers">
-               <div className="input-block">
-                  <label htmlFor="subject">Matérias</label>
-                  <input type="text" id="subject"/>
-               </div>
-
-               <div className="input-block">
-                  <label htmlFor="week_day">Dia da semana</label>
-                  <input type="text" id="week_day"/>
-               </div>
-
-               <div className="input-block">
-                  <label htmlFor="time">Horas</label>
-                  <input type="text" id="time"/>
-               </div>
+               <Input name="subject" label="Matéria" />
+               <Input name="week_day" label="Dia da semana" />
+               <Input type="time" name="time" label="Hora" />
             </form>
          </PageHeader>
 
